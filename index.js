38f1c46952ad3be8d808aa38c66304bc55b15dd4
Eric Lake <ericallenlake@gmail.com>
2022-06-26 21:17:07 -0600

    Starts questions

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
-const { Employee, Manager, Engineer, Intern } = require('./lib/classes');
+// const { Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
+// const fs = require('fs');
+const inquirer = require('inquirer')
+
+let expander = (employeeType) => {
+    switch(employeeType) {
+    case 'Manager':
+        return 'Employee office number'
+        break;
+    case 'Engineer':
+        return 'GitHub Username'
+        break;
+    case 'Intern':
+        return 'School/University'
+        break;
+    default:
+        return
+        break;
+    }
+}
+
+lastQuestion = input => expander(input)
+
+const questions = ['Enter employee name: ', 'Enter employee ID', 'Enter employee email address', 'Select employee role', lastQuestion]
 
 
 
-let eric = new Employee('eric', '001', 'ericlake@me.com');
-let danny = new Manager ('Danny', '002', 'teacher.danny.notreal@gmail.com', '101');
-let marlee = new Engineer('Marlee', '003', 'marleeahudson@gmail.com', 'marcelona')
-let isaiah = new Intern('Isaiah', '004', 'isaiah.example@gmail.com', 'University of Denver')
 
-console.log(danny.role)
-console.log(marlee.github)
-console.log(isaiah.email)
+
+function init() {
+    let finalQuestion = questions.pop()
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            console.log(data)
+            // writeToFile(`${data.title.toUpperCase()}-README`, generateMarkdown.generateMarkdown(data))
+        })
+        .then(console.log(finalQuestion('Manager')))
+}
+
+// Function call to initialize app
+init();
+
+
+
+// let eric = new Employee('eric', '001', 'ericlake@me.com');
+// let danny = new Manager ('Danny', '002', 'teacher.danny.notreal@gmail.com', '101');
+// let marlee = new Engineer('Marlee', '003', 'marleeahudson@gmail.com', 'marcelona')
+// let isaiah = new Intern('Isaiah', '004', 'isaiah.example@gmail.com', 'University of Denver')
+
+// console.log(danny.role)
+// console.log(marlee.github)
+// console.log(isaiah.email)
