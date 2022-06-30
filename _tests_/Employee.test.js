11947fe4dbf3d59237619832f3b090bb75a3fb1d
Eric Lake <ericallenlake@gmail.com>
2022-06-29 18:31:44 -0600

    Adds first test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,14 @@
+const Manager = require('../lib/classes')
+
+describe('Manager class', () => {
+    describe('Initialization', () => {
+        it('should take a name, id, email address, and office number', () => {
+            const manager = new Manager('Eric', 202, 'ericlake@me.com', 202)
+
+            expect(manager.id).toEqual(202)
+            expect(manager.name).toEqual('Eric');
+            expect(manager.email).toEqual('ericlake@me.com')
+            expect(manager.officeNumber).toEqual(202)
+        })
+    })
+})
