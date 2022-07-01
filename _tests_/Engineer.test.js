4c6415d6e925ff4658b8d8b356045979a4d7f723
Eric Lake <ericallenlake@gmail.com>
2022-06-30 22:02:18 -0600

    Adds engineer test

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,16 @@
+const {Engineer} = require('../lib/classes')
+
+
+
+describe('Engineer class', () => {
+    describe('Initialization', () => {
+        it('should take a name, id, email address, and github username', () => {
+            const engineer= new Engineer('Graham', 232, 'flyingburritos@gmail.com', 'burrito-delux')
+engineer
+            expect(engineer.id).toEqual(232)
+            expect(engineer.name).toEqual('Graham');
+            expect(engineer.email).toEqual('flyingburritos@gmail.com')
+            expect(engineer.github).toEqual('burrito-delux')
+        })
+    })
+})
